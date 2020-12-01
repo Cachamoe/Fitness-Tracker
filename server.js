453bbefa9cbd10b6c1bb454b233368b4b9f9b128
Caleb Chaney <67514046+Cachamoe@users.noreply.github.com>
2020-11-30 20:00:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression"); 
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const app = express();
@@ -13,7 +12,6 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(compression());
 
 // Require Routes
 app.use(require("./routes/api-routes"));
